16c575bc5d9f330e7e60626dfd9d030a447898ef
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-10-17 22:34:33 +0530

    added_InfoSection -> Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import './styles/Home.css'
 import { Link } from 'react-router-dom'
 
 import hero from './assets/home_hero.png'
+import info from './assets/home_info.png'
 const Home = () => {
     return (
         <div className='home'>
@@ -20,6 +21,18 @@ const Home = () => {
 
                 <div className="hero__img">
                     <img src={hero} alt="" />
+                </div>
+            </div>
+            <div className="info">
+                <div className="info__textbox">
+                    <div className="info__textbox__text">
+                        <span>hostel managment system</span>
+                        <h2>"Seamless Hostel Managment, Elevated Online"</h2>
+                        <p>"Hosteler" is an all-in-one hostel management solution designed to streamline and simplify the management of hostel facilities within educational institutes. With a custom page dedicated to each institute, Hosteler empowers institutions to effortlessly oversee and optimize hostel operations, catering to the specific needs of students and staff alike.</p>
+                    </div>
+                </div>
+                <div className="info__img">
+                    <img src={info} alt="" />
                 </div>
             </div>
         </div>
