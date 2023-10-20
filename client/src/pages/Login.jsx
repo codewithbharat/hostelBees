8690100356bac28ed315e6af9dee0ada0f5d1066
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-10-20 18:18:08 +0530

    added__loginForm -> Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
+import './styles/Login.css'
+import { Link } from 'react-router-dom'
+
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className="login">
+            <div className="login__form">
+                <form action="">
+                    <h2>log in</h2>
+                    <label>email/phone no.
+                        <input type="text" name="" id="" placeholder='email/phone no.' required />
+                    </label>
+
+                    <label>
+                        password
+                        <input type="password" placeholder='your password' required />
+                    </label>
+                    <div className="login__form__buttons">
+                        <input type="submit" value="login" />
+                        <Link className='a' to="/register">join now</Link>
+                    </div>
+                </form>
+            </div>
+            <div className="login__info"></div>
+        </div>
     )
 }
 
