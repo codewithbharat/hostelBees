5be7a83dd0f27c5dcb0857591fe8da2bca79bd89
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-11-03 19:33:52 +0530

    @client/src/pages/contact - created

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import './styles/Contact.css'
+
+import contactImg from './assets/contact_img.png'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className="contact">
+            <div className="contact__img">
+                <img src={contactImg} alt="" />
+            </div>
+            <div className="contact__form">
+
+                <form>
+                    <h2>Contact Us</h2>
+                    <div className="contact_form__labelGroup">
+                        <label>First Name
+                            <input type="text" name="" id="" placeholder='First Name' required />
+                        </label>
+                        <label>Last Name
+                            <input type="text" name="" id="" placeholder='First Name' required />
+                        </label>
+                    </div>
+                    <div className="contact_form__labelGroup">
+                        <label>Email
+                            <input type="text" name="" id="" placeholder='your@email.com' required />
+                        </label>
+                        <label>Phone No.
+                            <input type="text" name="" id="" placeholder='9162XXXXXX' required />
+                        </label>
+                    </div>
+                    <label>Message
+                        <textarea name="Message" id="" cols="30" rows="10" placeholder='Leave us a message...'></textarea>
+                    </label>
+
+                    <input className="contact__form__submit" type="submit" value="Send Message" />
+                </form>
+            </div>
+        </div>
     )
 }
 
