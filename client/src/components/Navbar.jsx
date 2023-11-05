c048fa00711047e185d0ff0357e0f77416ab0cc4
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-11-05 17:28:46 +0530

    navbar -> done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style/Navbar.css'
 
 import NavLogo from './assets/Nav_Logo.png'
-import NavhamburgerMenu from './assets/Nav_hamburger_menu.png'
 import { NavLink, Link } from 'react-router-dom'
 
+import { RxHamburgerMenu, RxCross1 } from 'react-icons/rx'
+
 const Navbar = () => {
+
+    const [toggle, setToggle] = useState(false);
+
     return (
         <>
             <div className="navbar">
@@ -18,14 +22,26 @@ const Navbar = () => {
                     </div>
                 </Link>
 
-                <div className="navbar__menu">
+
+                <div className="navbar__hamMenu" onClick={() => setToggle(!toggle)}>
+                    <span>
+                        {!toggle && <RxHamburgerMenu />}
+                    </span>
+
+                    <span>
+                        {toggle && <RxCross1 />}
+                    </span>
+                </div>
+
+
+                {toggle && <div className="navbar__menu">
                     <NavLink to="/" className="navbar__menu-link">Home</NavLink>
                     <NavLink to="/about" className="navbar__menu-link">About</NavLink>
                     <NavLink to="/contact" className="navbar__menu-link">Contact</NavLink>
                     <NavLink to="/register" className="navbar__menu-register">Join Now</NavLink>
 
-                </div>
-            </div>
+                </div>}
+            </div >
         </>
     )
 }
