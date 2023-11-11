59f121a9b6150434201b2a879fc560db5838f373
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-11-11 21:16:09 +0530

    favicon

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import Favicon from "react-favicon"
+
 //Importing react-router-dom
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <>
+    <Favicon url="../public/assets/favicon.ico" />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  </>
 )
