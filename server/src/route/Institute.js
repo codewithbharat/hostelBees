ff35490535e048b0452c64d1ec1abc57f5d9600e
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-11-22 17:53:46 +0530

    protected InstitutesRoutes

--- server/src/route/Institute.js
+++ server/src/route/Institute.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const { createInst, updateInst, getAllInst, getInstById, deleteInst } = require("../controller/institute");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.route('/inst').post(createInst);
-router.route('/inst/all').get(getAllInst);
-router.route('/inst/:id').get(getInstById);
-router.route('/inst/:id').put(updateInst);
-router.route('/inst/:id').delete(deleteInst);
+router.route('/inst/all').get(authMiddleware, getAllInst);
+router.route('/inst/:id').get(authMiddleware, getInstById);
+
+router.route('/inst').post(authMiddleware, createInst);
+router.route('/inst/:id').put(authMiddleware, updateInst);
+router.route('/inst/:id').delete(authMiddleware, deleteInst);
 
 
 module.exports = router;
