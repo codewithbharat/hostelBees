747953060db110870b3da111fdb9b65ca4e43d53
Bharat Ranjan <ranjanbharat54@gmail.com>
2023-12-11 16:39:18 +0530

    instDasboard/sidebar done

--- client/src/components/instComponents/Layout.jsx
+++ client/src/components/instComponents/Layout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Sidebar from './Sidebar'
 
 const Layout = ({ children }) => {
     return (
         <div class="flex flex-row h-screen">
-            <div class="basis-1/6  bg-slate-500">sidebar</div>
+            <div class="basis-1/6  bg-indigo-200">
+                <Sidebar />
+            </div>
             <div class="basis-5/6 bg-gray-800">
                 <div className="flex flex-col h-screen">
                     <div class="basis-1/12 bg-white">nav</div>
