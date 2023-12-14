e36dd23adbc52e5d066f1dde41451627a5290579
Abhinav Chandra <indiachandra02@gmail.com>
2023-12-15 01:16:54 +0530

    created -> Status Widgets

--- client/src/components/instComponents/Topbar.jsx
+++ client/src/components/instComponents/Topbar.jsx
@@ -4,7 +4,7 @@ import { FiSearch } from "react-icons/fi";
 
 const Topbar = () => {
   return (
-    <div className="flex flex-row w-full justify-between">
+    <div className="flex flex-row w-full h-full items-center justify-between">
         <div className="ml-4">
             <h1 className='text-xl'>
                 Overview
@@ -15,7 +15,7 @@ const Topbar = () => {
         </div>
  
         <div>
-          <div className="flex flex-row h-full items-center">
+          <div className="flex flex-row h-full pr-5 items-center">
             <input className="drop-shadow-md text-sm p-2 pl-2 pr-8 tracking-wider w-80 rounded-lg" type="text" placeholder='Search for hostels, warden, student, etc.' />
               <FiSearch className=' -ml-6 z-10 text-slate-600' />
             <CgProfile className='mr-8 ml-4 text-2xl rounded-full  text-slate-500' />
--- client/src/inst/InstDashboard.jsx
+++ client/src/inst/InstDashboard.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import Layout from "../components/instComponents/Layout"
 const InstDashboard = () => {
+
+    
+   const  StatusData = [
+        {
+            title : "Student",
+            strength : 200,
+            percentage : "50%"
+        },
+        {
+            title : "warden",
+            strength : 20,
+            percentage : "50%"
+        },
+        {
+            title : "Expenses",
+            strength : "30,30,00",
+            percentage : "50%"
+        },
+        {
+            title : "Notification",
+            strength : 200,
+            percentage : "50%"
+        },
+    ]
+
+    const StatusWidget = ({title , strength, percentage}) => {
+
+
+        return (
+            <div className=" w-56 h-28 shadow-md rounded-md flex flex-row justify-between p-5 m-10 items-center bg-white">
+                <div className="flex flex-col items-strech"> 
+                    <p className='text-lg'>{title}</p>
+                    <span className='text-sm'>{strength}</span>
+                </div>
+                <div className="flex items-center justify-center">
+                    {percentage}
+                </div>
+            </div>
+        )
+    }
+
     return (
         <Layout>
-            body
+            <div className="flex flex-row justify-between">
+                {
+                    StatusData.map(({title, strength, percentage}) => (
+                        <StatusWidget title={title} strength={strength} percentage={percentage} />
+                    ))
+                }
+            </div>
         </Layout>
     )
 }
