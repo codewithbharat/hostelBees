04129287bedf030dc556c17a6b4f7fdf3dbf4756
Bharat Ranjan <ranjanbharat54@gmail.com>
2023-12-16 14:35:49 +0530

    Topbar.jsx -> userDropdown

--- client/src/components/instComponents/Topbar.jsx
+++ client/src/components/instComponents/Topbar.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CgProfile } from "react-icons/cg";
 import { FiSearch } from "react-icons/fi";
+import { GrUserManager } from "react-icons/gr";
 
 const Topbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const UserDropdown = () => {
+    return (
+      // overlay
+      <div onClick={() => setIsOpen(!isOpen)} className='fixed top-0 right-0 bottom-0 left-0 h-full w-full z-50'>
+        <div className="absolute right-12 top-12 w-48 py-2 mt-2 bg-white rounded-md shadow-xl">
+          <div className="flex flex-col items-center mt-4 border-b-2 cursor-default ">
+            <GrUserManager size={32} />
+            <p className='font-semibold text-lg'>Bharat Ranjan</p>
+            <p className=' font-thin text-sm tracking-wide'>Administrator</p>
+            <p className=' font-extralight text-sm tracking-wider'>SPNREC, Araria</p>
+          </div>
+          <div className='my-2 border-b-2'>
+            <a href="#" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100">Your Profile</a>
+            <a href="#" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100">Settings</a>
+            <a href="#" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100">Sign out</a>
+          </div>
+          <div className='my-2 border-b-2'>
+            <a href="#" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100">Help & Support</a>
+            <a href="#" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100">Privacy & Policy</a>
+            <a href="#" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100">Term's conditons</a>
+          </div>
+          <p className="block cursor-default px-4 py-1 text-sm text-gray-70"> &copy; Hostelbees 2023</p>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="flex flex-row w-full h-full items-center justify-between">
       <div className="ml-8">
@@ -17,9 +48,10 @@ const Topbar = () => {
       <div>
         <div className="flex flex-row h-full pr-5 items-center">
           <input className="drop-shadow-lg text-sm p-2 pl-2 pr-8 tracking-wider w-80 rounded-lg" type="text" placeholder='Search for hostels, warden, student, etc.' />
-          <FiSearch className=' -ml-6 z-10 text-slate-600' />
-          <CgProfile className='mr-8 ml-4 text-2xl rounded-full  text-slate-500' />
+          <FiSearch size={18} className=' -ml-6 z-10 text-slate-600' />
+          <CgProfile onClick={() => setIsOpen(!isOpen)} size={30} className='mr-8 ml-4 rounded-full cursor-pointer  text-slate-500' />
         </div>
+        {isOpen && <UserDropdown />}
       </div>
     </div>
   )
