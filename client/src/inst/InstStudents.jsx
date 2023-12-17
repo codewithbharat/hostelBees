ee601c9b69497f6b2aa3f386d6b21cab398ed3cb
Bharat Ranjan <ranjanbharat54@gmail.com>
2023-12-17 12:52:30 +0530

    InstStudent.js -> addStudents

--- client/src/inst/InstStudents.jsx
+++ client/src/inst/InstStudents.jsx
@@ -4,6 +4,7 @@ import StudentList from '../components/instComponents/StudentList'
 import { BiMenuAltLeft } from "react-icons/bi";
 
 const InstStudents = () => {
+    const [showAddStudent, setShowAddStudent] = React.useState(false)
     const StudentData = [
         {
             index: 1,
@@ -60,6 +61,24 @@ const InstStudents = () => {
             room: 'A-101'
         }
     ]
+
+    const AddStudents = () => {
+        return (
+            <div className="absolute top-0 bottom-0 right-0 left-0 flex justify-center items-center bg-gray-900/50 z-10">
+                <div className="rounded-xl flex flex-col w-4/6 h-3/4 bg-gray-50 shadow-md p-4">
+                    <div className="basis-1/2 flex">
+                        <div className="basis-1/4 bg-slate-700"> 1</div>
+                        <div className="basis-3/4 bg-red-500">2
+
+                        </div>
+                    </div>
+                    <div className="basis-1/2">
+                        <button onClick={() => setShowAddStudent(false)}>save</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     return (
         <Layout>
             <div className='flex flex-row items-center mx-12'>
@@ -69,13 +88,16 @@ const InstStudents = () => {
                     <BiMenuAltLeft className='text-2xl mx-2' />
                 </div>
 
-                <div className='bg-green-600 font-semibold text-white ml-auto  px-2 py-1 rounded-md shadow-md shadow-slate-500/50'>
+                <div onClick={() => setShowAddStudent(true)} className=' cursor-pointer bg-green-600 font-semibold text-white ml-auto  px-2 py-1 rounded-md shadow-md shadow-slate-500/50'>
                     + Add Student
                 </div>
 
             </div>
 
             <StudentList students={StudentData} />
+            {
+                showAddStudent && <AddStudents />
+            }
 
         </Layout>
     )
