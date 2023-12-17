5484625cb9274efa929682af1d8914e10ce438ac
Bharat Ranjan <ranjanbharat54@gmail.com>
2023-12-17 14:26:36 +0530

    Revert "Client/development"

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,16 +1,11 @@
 import React, { useEffect } from 'react'
-import './styles/NotFound.css'
 
 const NotFound = () => {
     useEffect(() => {
         document.title = "Page Not Found"
     }, []);
     return (
-                <div className='NotFound'>
-
-                    <button>Go Back Home</button>
-                
-                </div>
+        <div>NotFound</div>
     )
 }
 
